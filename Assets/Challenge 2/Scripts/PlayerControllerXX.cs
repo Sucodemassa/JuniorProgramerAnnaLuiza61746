ed45bfeed2d6bd99ae239af6a17c92cb102735d4
Anna luiza 61746 <annacg0913@gmail.com>
2026-03-20 12:32:51 +0000

    Challenge 2 - Play Fetch

---
+++ Assets/Challenge 2/Scripts/PlayerControllerXX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerXX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float cooldownTime = 2.0f;
+    private float nextSpawnTime = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextSpawnTime)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+
+            nextSpawnTime = Time.time + cooldownTime;  
+        }
+    }
+}
